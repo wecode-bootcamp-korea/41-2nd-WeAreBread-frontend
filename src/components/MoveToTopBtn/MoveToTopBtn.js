fd5ca4fc72c0bbb49f0c6ffa2cfb3b2e2e7b1b1e
dalsong00 <index159@naver.com>
2023-02-02 11:32:55 +0900

    Add : MoveToTopBtn 레이아웃 및 기능 구현

---
+++ src/components/MoveToTopBtn/MoveToTopBtn.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BsArrowUpCircleFill } from 'react-icons/bs';
+
+const MoveToTopBtn = () => {
+  const moveToTop = () => {
+    window.scrollTo({ top: 0 });
+  };
+
+  return (
+    <div>
+      <MoveToTopIcon onClick={moveToTop} />
+    </div>
+  );
+};
+
+export default MoveToTopBtn;
+
+const MoveToTopIcon = styled(BsArrowUpCircleFill)`
+  position: fixed;
+  right: 40px;
+  bottom: 60px;
+  width: 50px;
+  height: 50px;
+  color: lightgray;
+  cursor: pointer;
+
+  &:hover {
+    color: #1e5858;
+  }
+`;
